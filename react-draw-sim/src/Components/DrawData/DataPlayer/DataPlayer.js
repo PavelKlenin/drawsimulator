ab93cba47b4f194699e6771849fe799f9b456590
PavelK <pashka_89@mail.ru>
2021-08-26 17:36:03 +0530

    - tooltips (react-tooltip, IconInfo); - рефакторинг, комментарии;

--- react-draw-sim/src/Components/DrawData/DataPlayer/DataPlayer.js
+++ react-draw-sim/src/Components/DrawData/DataPlayer/DataPlayer.js
@@ -1,22 +1,35 @@
-import { React } from "react";
+import { useState, React } from "react";
 import "../DrawData.scss";
+import InfoIcon from "./../../common/InfoIcon";
 
 const DataPlayer = (props) => {
+  const [clicked, setClicked] = useState(false);
+
   const onPlayerClick = () => {
-    !error && props.onPlayerClick(props.id);
+    if (!error) {
+      props.onPlayerClick(props.id);
+      setClicked(true); 
+    }
   };
 
   const error = props.error.repeatedPlayers.isValid ? false : true;
 
   return (
     <p
-      className={`player ${props.subs || error ? "disabledPlayer" : ""} ${
-        props.repeated || props.overflowed ? "errorPlayer" : ""
+      className={`player ${props.subs || error ? "disabledPlayer" : ""}  ${
+        props.repeated || props.filledBasket ? "errorPlayer" : ""
       }`}
       onClick={onPlayerClick}>
       {props.name}
-      {props.basket > 0 && (
+      {props.basket > 0 ? (
         <span className={`basket basket${props.basket}`}>{props.basket}</span>
+      ) : (
+        !error &&
+        !clicked &&
+        props.id === 1 && <InfoIcon tipName={props.basketTipName} />
+        // иконка появляется только у первого игрока и скрывается при нажатии на
+        // поле игрока, когда выводится значение "корзины" (и при наличии ошибки
+        // поля игрока), т.к. подсказка больше не нужна
       )}
     </p>
   );
